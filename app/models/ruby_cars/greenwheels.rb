187e71cec56903044ae71fd1e4c3d9e90a908eee
Benjamin Stolte <benjamin.stolte@innoz.de>
2016-07-25 14:31:28 +0200

    use threshold for greenwheels lat lon filter

--- app/models/ruby_cars/greenwheels.rb
+++ app/models/ruby_cars/greenwheels.rb
@@ -30,7 +30,7 @@ module RubyCars
         results = []
         stations.each do |station|
           station.fetch('locations').each do |attributes|
-            next if attributes.fetch('geo').first == 0.0 && attributes.fetch('geo').last == 0.0
+            next if attributes.fetch('geo').first.between?(-0.5, 0.5) && attributes.fetch('geo').last.between?(-0.5, 0.5)
             results << [station.fetch('name'),
                         attributes.fetch('geo').first,
                         attributes.fetch('geo').last,
